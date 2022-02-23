f63b8b781f46f2787971b0d2af19d6406bab7fdc
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-02-24 01:32:04 +0600

    login added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,69 @@
 import { useState } from 'react';
 import './App.css';
 import firebaseInitializeAuthentication from './Firebase/firebase.init';
+import { getAuth, createUserWithEmailAndPassword ,signInWithEmailAndPassword  } from "firebase/auth";
+
 firebaseInitializeAuthentication();
-
-
-
-
-
+const auth = getAuth();
 
 function App() {
+  //console.log(auth);
 
   const [email,setEmail] = useState('');
   const [password,setPassword] = useState('');
-
-  const handleRegistration = (e) =>{
-    console.log(email,password);
+  const [error,setError] = useState('');
+  const [isLogin,setIsLogin] = useState(false); 
+  const toggleLogin = (e) =>{
+    setIsLogin(e.target.checked);
+  }
+  const hanldeLogin = (e)=>{
     e.preventDefault();
+    console.log(email,password);
+    if(password.length<6){
+      setError('password should be at least 6 character long');
+      return;
+    }
+    // if(!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(password)){
+    //   setError('password is not strong enough');
+    //   return;
+    // }
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log('logged in');
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    setError(errorMessage);
+  });
+
+
+  }
+  const handleRegistration = (e) =>{
+    
+    e.preventDefault();
+    console.log(email,password);
+    if(password.length<6){
+      setError('password should be at least 6 character long');
+      return;
+    }
+    // if(!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(password)){
+    //   setError('password is not strong enough');
+    //   return;
+    // }
+   
+    createUserWithEmailAndPassword (auth, email, password)
+    .then((userCredential) => {
+  
+      const user = userCredential.user;
+      console.log(user);   
+     })
+     setError('');
+    
+    
   }
   const handleEmail = (e)=>{
     setEmail(e.target.value);
@@ -25,8 +73,8 @@ function App() {
   }
   return (
     <div className='mx-5'>
-    <h1>Please Register</h1>
-    <form onSubmit={handleRegistration}>
+    <h1>Please {isLogin?'Login':'Register'}</h1>
+    <form onSubmit={isLogin?hanldeLogin : handleRegistration}>
     <div className="row mb-3">
       <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
       <div className="col-sm-10">
@@ -38,19 +86,20 @@ function App() {
       <div className="col-sm-10">
         <input onBlur={handlePassword} type="password" className="form-control" id="inputPassword3"/>
       </div>
+      <div className='text-danger'>{error}</div>
     </div>
     
     <div className="row mb-3">
       <div className="col-sm-10 offset-sm-2">
         <div className="form-check">
-          <input className="form-check-input" type="checkbox" id="gridCheck1"/>
+          <input className="form-check-input" type="checkbox" id="gridCheck1" onChange={toggleLogin}/>
           <label className="form-check-label" htmlFor="gridCheck1">
-            Example checkbox
+            Already Registered
           </label>
         </div>
       </div>
     </div>
-    <button type="submit" className="btn btn-primary">Sign in</button>
+    <button type="submit" className="btn btn-primary">{isLogin?'Login':'Register'}</button>
   </form>
     </div>
   );
